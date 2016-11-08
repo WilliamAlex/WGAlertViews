466bcff1e11ad3bbba81750f747f43e3b04da74e
Uncle Alex <1509159596@qq.com>
2016-11-08 13:53:27 +0800

    Initial Commit

---
+++ WGAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGAlertView
+//
+//  Created by Alex William on 2016/11/8.
+//  Copyright © 2016年 WaiKa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
